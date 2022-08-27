a647e1e6e6f1d57d70a0d2d5a238b00868942595
Chauhan Vijaykumar <chauhanvijay81@gmail.com>
2022-08-27 12:01:29 +0530

    Adding Environment Object

--- src/main/java/com/vijay/springbootconfig/GreetingController.java
+++ src/main/java/com/vijay/springbootconfig/GreetingController.java
@@ -1,25 +1,25 @@
  package com.vijay.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.vijay.springbootconfig.profiles.SomeService;
 
 @RestController
 public class GreetingController {
 
 	/*
-	 * 08 Spring profiles explained
-	 * http://localhost:8080/profileinfo
+	 * 09 Using Environment object
+	 * http://localhost:8080/envdetails
 	 * 
+	 * From Video 10 we are using different project called spring-cloud-config-server
 	 */
 	@Autowired
-	SomeService ss;
+	Environment env;
 	
-	@GetMapping("/profileinfo")
-	public String getProfileInfo() {
-		return ss.toString();
+	@GetMapping("/envdetails")
+	public String getEnvDetails() {
+		System.out.println("Active Profiles: "+env.getActiveProfiles());
+		return env.toString();
 	}
-
 }
