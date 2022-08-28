e9fec238fe151dec1671e8ed3305a07fe95b9e91
Chauhan Vijaykumar <chauhanvijay81@gmail.com>
2022-08-28 08:12:08 +0530

    Updating Readme file

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## spring-boot-microservices-level3
 spring-boot-config
 v02  commits contains videos from 6 to 6.
+
+```
+http://localhost:8080/dbsettings
+```
+```
+http://localhost:8080/actuator/configprops
+```
--- src/main/java/com/vijay/springbootconfig/GreetingController.java
+++ src/main/java/com/vijay/springbootconfig/GreetingController.java
@@ -11,16 +11,11 @@ public class GreetingController {
 	 * 06 Configuration Properties
 	 * http://localhost:8080/actuator/configprops
 	 * http://localhost:8080/dbsettings
-	 * 
-	 * 07 is about yml file structure
 	 */
 	@Autowired
 	private DbSettings dbSettings;
 	
-
-	/*
-	 * This we will use in 11, 12 Video for spring cloud config
-	 */
+	
 	@GetMapping("/dbsettings")
 	public String getDbSettings() {
 		return dbSettings.toString();
