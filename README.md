4bbb1969379787d61b802994e0e2fe05650a5750
Chauhan Vijaykumar <chauhanvijay81@gmail.com>
2022-08-28 07:13:40 +0530

    Updating Readme file

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 ## spring-boot-microservices-level3
 spring-boot-config
 Main commits contains videos from 1 to 4.
+
+### 03 Using property file config with Spring Boot
+  
+ ```
+ http://localhost:8080/greeting 
+ ```
+ ```
+ http://localhost:8080/desc
+ ```
+  
+### 04 Using external property sources with Spring Boot
+  
+Method1 using overriding application.properties: create application.properties in same dir where jar exist. ( it will override properties insider properties) 
+
+```
+java -jar .\spring-boot-config-0.0.1-SNAPSHOT.jar
+```
+
+Method2 using command line argument: 
+
+```
+java -jar .\spring-boot-config-0.0.1-SNAPSHOT.jar --server.port=8081 --my.greeting="Hello from command line args"
+```
+  
--- src/main/java/com/vijay/springbootconfig/GreetingController.java
+++ src/main/java/com/vijay/springbootconfig/GreetingController.java
@@ -1,4 +1,4 @@
- package com.vijay.springbootconfig;
+package com.vijay.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,23 +9,27 @@ public class GreetingController {
 
 	/*
 	 * 03 Using property file config with Spring Boot
+	 * 
+	 * http://localhost:8080/greeting http://localhost:8080/desc
+	 * 
 	 * 04 Using external property sources with Spring Boot
 	 * 
-	 * Method1:
-	 * create application.properties in same dir where jar exist. ( it will override properties insider properties)
-	 * java -jar .\spring-boot-config-0.0.1-SNAPSHOT.jar
+	 * Method1 using overriding application.properties: create
+	 * application.properties in same dir where jar exist. ( it will override
+	 * properties insider properties) java -jar
+	 * .\spring-boot-config-0.0.1-SNAPSHOT.jar
 	 * 
-	 * Method2:
-	 * java -jar .\spring-boot-config-0.0.1-SNAPSHOT.jar --server.port=8081 --my.greeting="Hello from command line args"
+	 * Method2 using command line argument: java -jar
+	 * .\spring-boot-config-0.0.1-SNAPSHOT.jar --server.port=8081
+	 * --my.greeting="Hello from command line args"
 	 * 
 	 * 
-	 * http://localhost:8080/greeting
-	 * http://localhost:8080/desc
+	 * http://localhost:8080/greeting http://localhost:8080/desc
 	 */
-	
+
 	@Value("${my.greeting}")
 	private String greetingMessage;
-	
+
 	@GetMapping("/greeting")
 	public String greeting() {
 		return greetingMessage;
@@ -33,7 +37,7 @@ public class GreetingController {
 
 	@Value("${app.description}")
 	private String description;
-	
+
 	@GetMapping("/desc")
 	public String desc() {
 		return description;
