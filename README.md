8b8dedabecb64d7a4c31756d0ae9221f00e60e5b
Chauhan Vijaykumar <chauhanvijay81@gmail.com>
2022-08-29 23:08:40 +0530

    Updates changes

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 ## spring-boot-microservices-level3
 spring-boot-config
 v02  commits contains videos from 10 to 12.
+
+### 12 Setting up spring cloud config client
+
+To Start This We should have below things ready:
+- 1. Cloud Config Server up in port 8888
+- 2. Git Repo which connects by Cloud Config Server
+
+```
+http://localhost:8080/actuator/configprops
+```
+```
+http://localhost:8080/dbsettings
+```
--- src/main/java/com/vijay/springbootconfig/GreetingController.java
+++ src/main/java/com/vijay/springbootconfig/GreetingController.java
@@ -1,7 +1,6 @@
  package com.vijay.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,20 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
 	/*
-	 * 06 Configuration Properties
+	 * 12 Setting up spring cloud config client
+	 * 
+	 * To Start This We should have below things ready:
+	 * 	1. Cloud Config Server up in port 8888
+	 *  2. Git Repo which connects to Cloud Config Server
+	 *  
 	 * http://localhost:8080/actuator/configprops
 	 * http://localhost:8080/dbsettings
-	 * 
-	 * 07 is about yml file structure
 	 */
 	@Autowired
 	private DbSettings dbSettings;
 	
 	/*
-	 * This we will use in 11, 12 Video for spring cloud config
+	 * 12 Setting up spring cloud config client
 	 */
 	@GetMapping("/dbsettings")
 	public String getDbSettings() {
 		return dbSettings.toString();
 	}
 }
+ 
